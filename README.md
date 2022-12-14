5afca4dfacb3530a2908c1ed4d8e3449734653c0
terryleechen <73297091+terryleechen@users.noreply.github.com>
2022-12-14 06:43:35 -0700

    start setting up frontend

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# Tofu
+# Tofu Rental
 
-python -m uvicorn main:app --reload
+Frontend
+
+- React with Ts
+- Material UI
+- TailwindCSS
+- npm
+
+Backend
+
+- Mongodb
+- Uvicoirn - py -m uvicorn main:app --reload
+- Pymongo
+- FastAPI
+
+Notes:
 
 https://testdriven.io/blog/fastapi-mongo/
+
+Installations:
+
+- pip install -r requriement.txt
+- npm install @mui/material @emotion/react @emotion/styled
+- npm install react-router-dom
--- backend/routers.py
+++ backend/routers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Body, Request, HTTPException, status
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from typing import Optional, List
-from models import Car
+from models import Car, Branch, CarType, Employee, Customer, Reservation
 
 router = APIRouter()
 
@@ -15,4 +15,24 @@ async def get_cars(request: Request):
     cars = list(request.app.db["cars"].find())
     return cars
 
-    
+@router.get("/cars/{id}", response_description="Get a single car", response_model=Car)
+async def get_car(CarID: str, request: Request):
+    if (car := request.app.db["cars"].find_one({"CarID": CarID})) is not None:
+        return car
+
+    raise HTTPException(status_code=404, detail=f"Car {CarID} not found")
+
+@router.get("/branches", response_description="List all branches", response_model=List[Branch])
+async def get_branches(request: Request):
+    branches = list(request.app.db["branches"].find())
+    return branches
+
+@router.get("/carTypes", response_description="List all car types", response_model=List[CarType])
+async def get_carTypes(request: Request):
+    carTypes = list(request.app.db["carType"].find())
+    return carTypes
+
+@router.get("/employees", response_description="List all employees", response_model=List[Employee])
+async def get_employees(request: Request):
+    employees = list(request.app.db["employee"].find())
+    return employees
