e7277a8db8b1fa4c92c237d3c97ccfbdfc282e7e
terryleechen <73297091+terryleechen@users.noreply.github.com>
2022-12-16 05:58:03 -0700

    work on landing page top brands

--- frontend/src/Components/Layout/LandingPage.tsx
+++ frontend/src/Components/Layout/LandingPage.tsx
@@ -12,6 +12,7 @@ const navigation = [
 
 function LandingPage() {
   return (
+    // NavBar
     <div className="min-h-full">
       <div className="relative">
         <div className="pt-2 mx-auto max-w-full sm:px-6 lg:px-3">
@@ -96,6 +97,54 @@ function LandingPage() {
                     Rent Now
                   </a>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Logo Cloud */}
+      <div className="pt-2 mx-auto max-w-full sm:px-6 lg:px-3">
+        <div className="bg-orange-100 sm:rounded-2xl">
+          <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
+            <p className="text-center text-base font-semibold text-black">
+              Our Top 5 Trusted Fleet brands
+            </p>
+            <div className="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
+              <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+                <img
+                  className="h-12"
+                  src={require("./Honda.png")}
+                  alt="Honda"
+                />
+              </div>
+              <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+                <img
+                  className="h-10"
+                  src={require("./Volvo.png")}
+                  alt="Mirage"
+                />
+              </div>
+              <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+                <img
+                  className="h-12"
+                  src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
+                  alt="StaticKit"
+                />
+              </div>
+              <div className="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">
+                <img
+                  className="h-12"
+                  src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
+                  alt="Transistor"
+                />
+              </div>
+              <div className="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
+                <img
+                  className="h-12"
+                  src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
+                  alt="Workcation"
+                />
               </div>
             </div>
           </div>
